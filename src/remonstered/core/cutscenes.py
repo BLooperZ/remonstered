79afab6ed9656ab3a7c7a13635a6f3ed140a49e3
BLooperZ <blooperz@users.noreply.github.com>
2024-08-07 20:47:27 +0300

    upgrade project dependencies

--- src/remonstered/core/cutscenes.py
+++ src/remonstered/core/cutscenes.py
@@ -1,28 +1,27 @@
 import concurrent.futures
 import functools
-import io
 import itertools
 import os
+import pathlib
 import subprocess
 import sys
-from struct import Struct
+from collections.abc import Iterable, Iterator
 from contextlib import contextmanager
-from typing import Iterable
+from struct import Struct
 
-from nutcracker.compress_san import strip_compress_san
-from nutcracker.smush.preset import smush
+from nutcracker.smush import anim
+from nutcracker.smush.compress import strip_compress_san
 
 from . import lpak
 from .missing import closed_tempfile_name
-
 
 UINT32LE = Struct('<I')
 G_PAK = None
 
 
 @contextmanager
-def suppress_stdout():
-    with open(os.devnull, "w") as devnull:
+def suppress_stdout() -> Iterator[None]:
+    with open(os.devnull, 'w') as devnull:
         old_stdout = sys.stdout
         sys.stdout = devnull
         try:
@@ -72,17 +71,13 @@ def extract_ogv_audio(source: bytes, dest: str) -> None:
             sys.exit(1)
 
 
-def get_smush_offsets(res):
-    with io.BytesIO(res) as stream:
-        anim = smush.assert_tag('ANIM', smush.untag(stream))
-        assert stream.read() == b''
-
-    _, *frames = smush.read_chunks(anim)
-    for (offset, _) in frames:
-        yield offset + 8
+def get_smush_offsets(res: bytes) -> Iterator[int]:
+    _, frames = anim.parse(anim.from_bytes(memoryview(res)))
+    for elem in frames:
+        yield elem.attribs['offset'] + 8
 
 
-def get_base_size(pak: lpak.LPakArchive, fname: str):
+def get_base_size(pak: lpak.LPakArchive, fname: str) -> int:
     no_file = lpak.LPAKFileEntry(0, 0, 0, 0, 0)
     basename = os.path.basename(fname)
     simplename, ext = os.path.splitext(basename)
@@ -96,24 +91,30 @@ def get_base_size(pak: lpak.LPakArchive, fname: str):
     )
 
 
-def compress_single(pak: lpak.LPakArchive, fname: str, output_dir: str = '.'):
-    basename = os.path.basename(fname)
-    simplename, ext = os.path.splitext(basename)
+def compress_single(
+    pak: lpak.LPakArchive,
+    fname: str | os.PathLike[str],
+    output_dir: str | os.PathLike[str] = '.',
+) -> int:
+    fname = pathlib.Path(fname)
+    basename = fname.name
+    simplename = fname.stem
+    output_dir = pathlib.Path(output_dir)
 
     videohd = next(pak.iglob(os.path.join('videohd', f'{simplename}.ogv')), None)
     if videohd:
         # override SAN file with compressed version
-        with pak.open(fname, 'rb') as res, suppress_stdout():
-            data = strip_compress_san(res)
+        with pak.open(str(fname), 'rb') as res, suppress_stdout():
+            cont = anim.from_bytes(memoryview(res.read()))
+            data = strip_compress_san(cont)
 
-        directory = os.path.join(output_dir, os.path.basename(os.path.dirname(fname)))
+        directory = output_dir / fname.parent.name
         os.makedirs(directory, exist_ok=True)
-        with open(os.path.join(directory, basename), 'wb') as out:
-            out.write(data)
+        (directory / basename).write_bytes(data)
 
         flubase = f'{simplename}.flu'
         flufile = next(
-            pak.iglob(os.path.join(os.path.dirname(fname), flubase)),
+            pak.iglob(str(fname.parent / flubase)),
             None,
         )
         if flufile:
@@ -121,33 +122,30 @@ def compress_single(pak: lpak.LPakArchive, fname: str, output_dir: str = '.'):
                 flu = res.read(0x324)
                 flurest = res.read()
 
-            with pak.open(fname, 'rb') as res:
+            with pak.open(str(fname), 'rb') as res:
                 raw_content = res.read()
             assert flurest == b''.join(
                 UINT32LE.pack(offset) for offset in get_smush_offsets(raw_content)
             )
 
-            with open(os.path.join(directory, flubase), 'wb') as out:
-                out.write(flu)
-                out.write(
-                    b''.join(
-                        UINT32LE.pack(offset) for offset in get_smush_offsets(data)
-                    )
-                )
+            (directory / flubase).write_bytes(
+                flu
+                + b''.join(UINT32LE.pack(offset) for offset in get_smush_offsets(data))
+            )
 
         # extract audio stream from HD video
         with pak.open(videohd, 'rb') as vid:
             stream = vid.read()
-        extract_ogv_audio(stream, os.path.join(directory, f'{simplename}.ogg'))
-    return get_base_size(pak, fname)
+        extract_ogv_audio(stream, str(directory / f'{simplename}.ogg'))
+    return get_base_size(pak, str(fname))
 
 
-def init_worker(archive_name: str):
+def init_worker(archive_name: str) -> None:
     global G_PAK
     G_PAK = lpak.LPakArchive(archive_name)
 
 
-def convert_worker(fname: str, output_dir: str = '.'):
+def convert_worker(fname: str, output_dir: str = '.') -> int:
     global G_PAK
     assert G_PAK is not None
     return compress_single(G_PAK, fname, output_dir)
@@ -155,7 +153,7 @@ def convert_worker(fname: str, output_dir: str = '.'):
 
 def compress_and_convert_cutscenes(
     pak: lpak.LPakArchive, files: Iterable[str] = (), output_dir: str = '.'
-):
+) -> Iterator[int]:
     worker = functools.partial(convert_worker, output_dir=output_dir)
     with concurrent.futures.ProcessPoolExecutor(
         initializer=init_worker, initargs=(pak.path,)
@@ -163,12 +161,15 @@ def compress_and_convert_cutscenes(
         try:
             results = executor.map(worker, files)
             yield from results
-        except KeyboardInterrupt as kbi:
+        except KeyboardInterrupt:
             executor.shutdown(wait=False)
-            raise kbi
+            raise
 
 
-def convert_cutscenes(pak: lpak.LPakArchive, output_dir: str = '.'):
+def convert_cutscenes(
+    pak: lpak.LPakArchive,
+    output_dir: str = '.'
+) -> Iterator[tuple[str, tuple[Iterator[int], int]]]:
     patterns = {'video/*.san', 'data/*.san'}
     files = set(
         itertools.chain.from_iterable(pak.iglob(pattern) for pattern in patterns)
@@ -176,9 +177,12 @@ def convert_cutscenes(pak: lpak.LPakArchive, output_dir: str = '.'):
     if len(files) > 0:
         action = 'Converting cutscenes...'
         total_size = sum(get_base_size(pak, fname) for fname in files)
-        yield action, (
-            compress_and_convert_cutscenes(pak, files, output_dir),
-            total_size,
+        yield (
+            action,
+            (
+                compress_and_convert_cutscenes(pak, files, output_dir),
+                total_size,
+            ),
         )
 
 
