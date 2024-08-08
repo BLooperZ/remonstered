3e2449fca18585c2dbf9bc0514434acc212ec0ee
BLooperZ <blooperz@users.noreply.github.com>
2024-08-08 13:29:44 +0300

    simplify multiple options in localization

--- src/remonstered/core/localize.py
+++ src/remonstered/core/localize.py
@@ -1,5 +1,8 @@
 import io
 import struct
+import sys
+from collections import defaultdict
+from typing import IO
 
 from nutcracker.sputm.build import rebuild_resources
 from nutcracker.sputm.schema import SCHEMA
@@ -11,23 +14,22 @@ from nutcracker.sputm.strings import (
 )
 from nutcracker.sputm.tree import narrow_schema, open_game_resource
 
-
 UINT32BE = struct.Struct('>I')
 UINT32LE = struct.Struct('<I')
 
 
-def read_uint32be(stream):
+def read_uint32be(stream: IO[bytes]) -> int:
     return UINT32BE.unpack(stream.read(UINT32BE.size))[0]
 
 
-def read_uint32le(stream):
+def read_uint32le(stream: IO[bytes]) -> int:
     return UINT32LE.unpack(stream.read(UINT32LE.size))[0]
 
 
 def read_index(stream):
     num = read_uint32le(stream)
     for _ in range(num):
-        eid = read_uint32le(stream)
+        eid = stream.read(4)
 
         offs = [(read_uint32le(stream), read_uint32le(stream)) for _ in range(5)]
         yield (eid, offs)
@@ -40,14 +42,11 @@ def create_loc_map(stream):
     data = stream.read(size)
     assert stream.read() == b''
 
-    loc_map = {}
+    loc_map = defaultdict(list)
 
     with io.BytesIO(data) as st:
         ind = list(read_index(st))
         for eid, offs in ind:
-            # # commented out for duplicate entries with same eid for 'toilet' in DOTT
-            # # in spanish, one of them is 'aseo' while other are 'letrina'
-            # assert eid not in loc_map, eid
             entry = []
             for off, ln in offs:
                 assert off + 4 == st.tell(), (off, st.tell())
@@ -56,10 +55,21 @@ def create_loc_map(stream):
                     assert msg[-1] == 0, msg
                     msg = msg[:-1]
                 entry.append(msg)
-            
-            loc_map[eid] = entry + loc_map.get(eid, [])
+            assert len(entry) == 5, entry
+            # # commented out for duplicate entries with same eid for 'toilet' in DOTT
+            # # in spanish, one of them is 'aseo' while other are 'letrina'
+            # assert eid not in loc_map, (eid, entry, loc_map[eid])
+            loc_map[eid].append(entry)
+        assert st.read() == b''
 
     return loc_map
+
+
+def dump_loc_map(loc_map, file=sys.stdout):
+    for key, val in loc_map.items():
+        print(f'{key.hex(" ").upper()}:', file=file)
+        for v in val:
+            print(f'- {v}', file=file)
 
 
 langmap = {
@@ -88,17 +98,23 @@ def translate_game_resource(filename, archive, lang):
 
     with archive.open('localization/ClassicLoc.bin', 'rb') as clf:
         loc_map = create_loc_map(clf)
-        useful_loc_map = {val[0]: val for _, val in loc_map.items()}
+        useful_loc_map = {val[0][0]: val for _, val in loc_map.items()}
+
+    # dump loc_map
+    with open('loc_map.txt', 'w') as f:
+        dump_loc_map(loc_map, f)
 
     strings = []
 
     for msg in get_all_scripts(root, script_ops, script_map):
         translated = msg
         if msg in useful_loc_map:
-            translated = useful_loc_map[msg][langmap[lang]]
-            if len(useful_loc_map[msg]) > len(langmap):
-                useful_loc_map[msg] = useful_loc_map[msg][len(langmap):]
-        # translated = useful_loc_map.get(msg, [msg] * len(langmap))[langmap[lang]]
+            option = 0
+            num_options = len(useful_loc_map[msg])
+            if num_options > 1:
+                print(f'Multiple options found for {msg}: {num_options}')
+            # TODO: Figure out which option should be used when multiple options are available
+            translated = useful_loc_map[msg][option][langmap[lang]]
         strings.append(translated)
 
     updated_resource = list(
